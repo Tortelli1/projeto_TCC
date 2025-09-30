e8909441155ce04db70759801db2df8d9239736b
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-09-30 19:51:28 -0300

    Ajustado package

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'yolo_realsense'
 
@@ -7,14 +9,21 @@ setup(
     version='0.1.0',
     packages=[package_name],
     package_dir={'': 'src'},
-    install_requires=['ultralytics', 'opencv-python', 'numpy', 'pyrealsense2'],
+    install_requires=['setuptools', 'ultralytics', 'opencv-python', 'numpy', 'pyrealsense2'],
+    zip_safe=True,
+    maintainer='Marco Tortelli',
+    maintainer_email='marcopazinitortelli@gmail.com',
+    description='YOLOv8 + Intel RealSense integration for ROS2',
+    license='MIT',
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml'])
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     entry_points={
         'console_scripts': [
-            'yolo_realsense_node = yolo_realsense.yolo_detector:main'
+            # executável ROS2 -> módulo Python
+            'yolo_realsense_node = yolo_realsense.main:main',
         ],
     },
 )
